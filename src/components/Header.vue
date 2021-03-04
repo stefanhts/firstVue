7ee2105cf65ccee7a13c7e43738c8d7574db97e0
stefan <stefan.heller00@gmail.com>
2021-03-04 12:21:10 -0500

    Commit of initial component additions

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color = "Green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default{
+    name:"Header",
+    components: {
+        Button,
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'Hello World'
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom:20px;
+}
+</style>
